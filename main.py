c0101b7df5aff3b3051881cceef2342879a4dd7f
oiivantsov <oiivantsov@outlook.com>
2024-01-22 10:43:47 +0200

    4 done

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
 "sandbox"
-
-largest = -10000000000000000000
-smallest = 10000000000000000000
-
-while 1:
-    l = input("type in an integer: ")
-
-    if (l.lstrip("-").isdigit()):
-        l = int(l)
-        if (l > largest):
-            largest = l
-        if (l < smallest):
-            smallest = l
-    else:
-        print("largest number: ", largest, "smallest number: ", smallest)
-        break
